69fa2cc961a47a0795f07fc352415890dc947bb1
Pedro Grigorio <pedrooh.hxh@gmail.com>
2023-07-07 10:48:54 -0300

    persist time-intervals and create availability date route

--- src/pages/register/time-intervals/index.page.tsx
+++ src/pages/register/time-intervals/index.page.tsx
@@ -101,7 +101,11 @@ export default function TimeIntervals() {
   async function handleSetTimeIntervals(data: any) {
     const { intervals } = data as TimeIntervalsFormOutput
 
-    await api.post('/users/time-intervals', { intervals })
+    console.log(intervals)
+
+    await api.post('/users/time-intervals', {
+      intervals,
+    })
     await router.push('/register/update-profile')
   }
 
