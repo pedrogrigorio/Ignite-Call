d8488eafb548d885780dc3e3a541ce58ab578bdc
Pedro Grigorio <pedrooh.hxh@gmail.com>
2023-07-09 12:50:32 -0300

    SEO

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -4,29 +4,38 @@ import { Container, Hero, Preview } from './styles'
 import previewImage from '@/assets/app-preview.png'
 import Image from 'next/image'
 import { ClaimUsernameForm } from './components/ClaimUsernameForm'
+import { NextSeo } from 'next-seo'
 
 export default function Home() {
   return (
-    <Container>
-      <Hero>
-        <Heading size="4xl">Agendamento descomplicado</Heading>
-        <Text size="xl">
-          Conecte seu calendário e permita que as pessoas marquem agendamentos
-          no seu tempo livre.
-        </Text>
+    <>
+      <NextSeo
+        title="Descomplique sua agenda | Ignite Call"
+        description="Conecte seu calendário e permita que as pessoas marquem agendamentos
+        no seu tempo livre."
+      />
 
-        <ClaimUsernameForm />
-      </Hero>
+      <Container>
+        <Hero>
+          <Heading size="4xl">Agendamento descomplicado</Heading>
+          <Text size="xl">
+            Conecte seu calendário e permita que as pessoas marquem agendamentos
+            no seu tempo livre.
+          </Text>
 
-      <Preview>
-        <Image
-          src={previewImage}
-          alt="preview"
-          height={400}
-          quality={100}
-          priority
-        />
-      </Preview>
-    </Container>
+          <ClaimUsernameForm />
+        </Hero>
+
+        <Preview>
+          <Image
+            src={previewImage}
+            alt="preview"
+            height={400}
+            quality={100}
+            priority
+          />
+        </Preview>
+      </Container>
+    </>
   )
 }
